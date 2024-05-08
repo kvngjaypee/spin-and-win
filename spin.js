ddd0135b3613c7c4ef0b2bf1131c4b861e1f9164
kvngjaypee <johnpaulanyanwu17@gmail.com>
2024-05-08 23:14:59 +0100

    updated this file with few lines of code

--- spin.js
+++ spin.js
@@ -8,6 +8,23 @@
 
 const prompt = require("prompt-sync")();
 
+const ROWS = 3;
+const COLS = 3;
+
+const SYMBOLS_COUNT = {
+  A: 2,
+  B: 4,
+  C: 6,
+  D: 8,
+};
+
+const SYMBOLS_vALUES = {
+  A: 5,
+  B: 4,
+  C: 3,
+  D: 2,
+};
+
 const deposit = () => {
   while (true) {
     const depositAmount = prompt("Deposit an amount: ");
@@ -21,5 +38,31 @@ const deposit = () => {
   }
 };
 
-const depositAmount = deposit();
-console.log(depositAmount);
+const getNumberOfLines = () => {
+  while (true) {
+    const lines = prompt("Enter the number of lines to bet ranging from 1-3: ");
+    const numberOfLines = parseFloat(lines);
+
+    if (isNaN(numberOfLines) || numberOfLines <= 0 || numberOfLines > 3) {
+      console.log("Sorry, that an invalid number of lines... Try again");
+    } else {
+      return numberOfLines;
+    }
+  }
+};
+
+const getBet = (balance) => {
+  while (true) {
+    const bet = prompt("Enter the total bet: ");
+    const numberOfBet = parseFloat(bet);
+
+    if (isNaN(numberOfBet) || numberOfBet <= 0 || numberOfBet > balance) {
+      console.log("Invalid bets... Try again");
+    } else {
+      return numberOfBet;
+    }
+  }
+};
+let balance = deposit();
+const numberOfLines = getNumberOfLines();
+const bet = getBet(balance);
